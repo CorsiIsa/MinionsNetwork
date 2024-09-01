d9dacbf3ee8965051c8d8ce675b122ff277b7b7a
CorsiIsa <icorsi15@gmail.com>
2024-09-01 16:20:04 -0300

    fix: realizando ajustes front

--- src/screens/NewPost.tsx
+++ src/screens/NewPost.tsx
@@ -23,15 +23,15 @@ const headerProps = {
 
 const userProps = {
   source: 'https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  username: 'Anderson Godoy',
-  userSlug: '@AndersonGodoy'
+  username: 'Stuart',
+  userSlug: '@Stuart'
 }
 export default function NewPost({  route }: Props) {
   return (
     <SafeAreaView >
       <ScrollView>
         <Header source={headerProps.source} />
-        <User  userId='1234' username='Anderson Godoy' source={userProps.source} userSlug={userProps.userSlug} />
+        <User  userId='1234' username='Stuart' source={userProps.source} userSlug={userProps.userSlug} />
         <TituloPostagem/>
         <ImagePost/>
         <BotoesNewPost/>
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -26,8 +26,8 @@ const headerProps = {
 
 const userProps = {
   source: 'https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  username: 'Anderson Godoy',
-  userSlug: '@AndersonGodoy'
+  username: 'Stuart',
+  userSlug: '@Stuart'
 }
 
 
@@ -39,7 +39,7 @@ export default function Profile({ navigation, route }: Props) {
     <SafeAreaView>
       <ScrollView>
         <Header source={headerProps.source} />
-        <User userId={userId} username='Anderson Godoy' source={userProps.source} userSlug={userProps.userSlug} />
+        <User userId={userId} username='Stuart' source={userProps.source} userSlug={userProps.userSlug} />
         <Bio bioText='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt in libero quis vulputate. Donec et malesuada nisi, vitae condimentum mi. Nullam tincidunt fermentum nisl id congue. Pellentesque at erat id ipsum commodo tincidunt. Quisque ut nunc vel libero ultrices ornare.' bioTitle='Bio' />
         <Preferences />
         <Info />
